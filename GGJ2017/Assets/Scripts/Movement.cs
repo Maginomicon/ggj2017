22a2165727e46e557bd412e1ccf911f258010b9e
Arthur-Hinsvark <ajhinsvark@gmail.com>
2017-01-21 11:07:37 -0600

    Force based movement and Keyboard control

--- GGJ2017/Assets/Scripts/Movement.cs
+++ GGJ2017/Assets/Scripts/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour
 {
 
     public float speed = 1.0f;
+    public float maxSpeed = 20f;
 
     //Controller
     public int playerNum = 1;
@@ -21,12 +22,15 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Inputs from left stick
         float move_h = Input.GetAxis("L_XAxis_" + playerNum);
         float move_v = Input.GetAxis("L_YAxis_" + playerNum);
 
+        //Inputs from right stick
         float rot_x = Input.GetAxis("R_XAxis_" + playerNum);
         float rot_y = Input.GetAxis("R_YAxis_" + playerNum);
 
+        //Rotation
         if (rot_x!= 0 ||
             rot_y != 0)
         {
@@ -37,14 +41,24 @@ public class Movement : MonoBehaviour
                 Quaternion.AngleAxis(angle, Vector3.forward), 180f);
         }
 
+        //Movement
         if(move_h != 0 ||
             move_v != 0)
         {
-            rg2D.velocity = new Vector2(Mathf.Lerp(0, move_h * speed, 0.8f),
-            Mathf.Lerp(0, move_v * speed, 0.8f));
+            //Physics movement
+            
+            rg2D.AddForce(new Vector2(move_h * speed, move_v * speed));
+            if(rg2D.velocity.magnitude >= maxSpeed)
+            {
+                rg2D.velocity = rg2D.velocity.normalized * maxSpeed;
+            }
+
+            //Direct control Movement
+            //rg2D.velocity = new Vector2(Mathf.Lerp(0, move_h * speed, 0.8f),
+            //Mathf.Lerp(0, move_v * speed, 0.8f));
         }else
         {
-            rg2D.velocity = rg2D.velocity.normalized* speed;
+            //rg2D.velocity = rg2D.velocity.normalized* speed;
         }
         
 
